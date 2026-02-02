859c10ae8f8edeee7c5547058cc0c6c24941e551
fxpasst1 <t1@zcall.tk>
2026-02-02 15:41:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
 
+# 启动 x-tunnel (后台运行)
+echo "Starting x-tunnel..."
+x-tunnel ${XTUNNEL_ARGS} &
+
+# 启动 cloudflared (前台运行，如果它挂了，容器就会退出)
+echo "Starting cloudflared..."
+if [ -n "$CF_TOKEN" ]; then
+    exec cloudflared tunnel --no-autoupdate run --token ${CF_TOKEN}
+else
+    echo "Error: CF_TOKEN is not set."
+    exit 1
+fi
